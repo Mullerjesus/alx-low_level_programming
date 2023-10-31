a5caecd742fcd2679198556f19e346cb67975d08
Muluken <mulukeneshetu70@gmail.com>
2023-10-30 18:12:53 -0700

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,60 +8,46 @@
 #define BUF_SIZE 1024
 
 int main(int argc, char *argv[])
-{
-int fd_from, fd_to, ret;
+
+{int fd_from, fd_to, ret;
 char buf[BUF_SIZE];
 ssize_t num_read, num_written;
-
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+return (97);
 }
-
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+return (98);
 }
-
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR;
+S_IWUSR | S_IRGRP | S_IROTH);
 if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
+{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+return (99);
 }
-
 while ((num_read = read(fd_from, buf, BUF_SIZE)) > 0)
-{
-num_written = write(fd_to, buf, num_read);
+{num_written = write(fd_to, buf, num_read);
 if (num_written != num_read)
 {
 dprintf(STDERR_FILENO, "Error: Write to %s failed\n", argv[2]);
-exit(99);
+return (99);
 }
 }
-
 if (num_read == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+return (98);
 }
-
 ret = close(fd_from);
 if (ret == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
+{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+return (100);
 }
-
 ret = close(fd_to);
 if (ret == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
+{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+return (100);
 }
-
-exit(0);
+return (0);
 }
