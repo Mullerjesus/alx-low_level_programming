1a7d71cd9ab3b80c878f0746c0378e7e240349de
Muluken <mulukeneshetu70@gmail.com>
2023-11-01 13:13:55 -0700

    C - File I/O

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -17,7 +17,7 @@ if (ac != 2)
 dprintf(2, "Usage: %s filename\n", av[0]);
 exit(1);
 }
-n = read_textfile(av[0], 2000);
+n = read_textfile(av[2], 114);
 printf("\n(printed chars: %li)\n", n);
 n = read_textfile(av[1], 1024);
 printf("\n(printed chars: %li)\n", n);
