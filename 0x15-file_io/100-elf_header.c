217d45b7467e12bc8f2d4c0487ec5c4c4a9489b5
Muluken <mulukeneshetu70@gmail.com>
2023-11-01 15:06:49 -0700

    C - File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,46 +1,137 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
 /**
-*main - algo mucho muy
-*@argc: no lo se
-*@argv: esto esta hecho para tener mas puntos
-*Return: no funciona
+* check_elf - Checks if the file is an ELF file.
+* @e_ident: A pointer to an array containing the ELF class.
 */
-int main (int argc, char** argv[])
+void check_elf(unsigned char *e_ident)
 {
-int f;
-ssize_t s;
-char buffer[5];
-char elf[1];
+if (e_ident[EI_MAG0] != ELFMAG0 ||
+e_ident[EI_MAG1] != ELFMAG1 ||
+e_ident[EI_MAG2] != ELFMAG2 ||
+e_ident[EI_MAG3] != ELFMAG3)
+{
+dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+exit(98);
+}
+}
+/**
+* print_data - Prints the data encoding of an ELF header.
+* @e_ident: A pointer to an array containing the ELF class.
+*/
+void print_data(unsigned char *e_ident)
+{
+printf("  Data:                             ");
+switch (e_ident[EI_DATA])
+{
+case ELFDATANONE:
+printf("Unknown data format\n");
+break;
+case ELFDATA2LSB:
+printf("2's complement, little endian\n");
+break;
+case ELFDATA2MSB:
+printf("2's complement, big endian\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_DATA]);
+}
+}
+/**
+* print_version - Prints the version of an ELF header.
+* @e_ident: A pointer to an array containing the ELF class.
+*/
+void print_version(unsigned char *e_ident)
+{
+printf("  Version:                          ");
+if (e_ident[EI_VERSION] == EV_CURRENT)
+printf("%d (current)\n", e_ident[EI_VERSION]);
+else
+printf("%d\n", e_ident[EI_VERSION]);
+}
+/**
+* print_type - Prints the type of an ELF header.
+* @e_type: The type of the ELF header.
+* @e_ident: A pointer to an array containing the ELF class.
+*/
+void print_type(uint16_t e_type, unsigned char *e_ident)
+{
+printf("  Type:                             ");
+if (e_ident[EI_CLASS] == ELFCLASS32)
+{
+switch (e_type)
+{
+case ET_NONE:
+printf("NONE (None)\n");
+break;
+case ET_REL:
+printf("REL (Relocatable file)\n");
+break;
+case ET_EXEC:
+printf("EXEC (Executable file)\n");
+break;
+case ET_DYN:
+printf("DYN (Shared object file)\n");
+break;
+case ET_CORE:
+printf("CORE (Core file)\n");
+break;
+default:
+printf("<unknown: %x>\n", e_type);
+}
+}
+}
+/**
+* main - Entry point.
+* @argc: The number of arguments passed to the program.
+* @argv: An array of pointers to the arguments.
+* Return: 0 on success, 98 on failure.
+*/
+int main(int argc, char **argv)
+{
+int fd;
+Elf32_Ehdr elf_header;
+unsigned char *e_ident = elf_header.e_ident;
 if (argc != 2)
 {
-dprintf(STDERR_FILENO, "Usage: cp file\n");
-return (97);
+dprintf(STDERR_FILENO, "Usage: %s <elf_filename>\n", argv[0]);
+exit(98);
 }
-
-f = open(char**[1], O_RDWR);
-
-if (f == -1)
+fd = open(argv[1], O_RDONLY);
+if (fd == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't open from file %s\n", argv[1]);
-return (98);
+dprintf(STDERR_FILENO, "Error: Cannot read file %s\n", argv[1]);
+exit(98);
 }
-
-s = read(f, buffer, 4);
-
-if (s == -1)
+if (read(fd, &elf_header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-return (98);
+dprintf(STDERR_FILENO, "Error: Cannot read ELF header\n");
+exit(98);
 }
-
-s = lseek(f, 1, SEEK_SET);
-
-s = read(f, elf, 1);
-
-if (elf[0] != 'E')
-return (98);
-
-close(f);
+check_elf(e_ident);
+printf("ELF Header:\n");
+printf("  Class:                            ");
+switch (e_ident[EI_CLASS])
+{
+case ELFCLASSNONE:
+printf("none\n");
+break;
+case ELFCLASS32:
+printf("ELF32\n");
+break;
+case ELFCLASS64:
+printf("ELF64\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+}
+print_data(e_ident);
+print_version(e_ident);
+if (e_ident[EI_CLASS] == ELFCLASS32)
+print_type(elf_header.e_type, e_ident);
+close(fd);
 return (0);
 }
