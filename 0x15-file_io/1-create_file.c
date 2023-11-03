c153d959c7a063ef40e4fd8ccf30f8f87e8565a2
Muluken <mulukeneshetu70@gmail.com>
2023-11-03 15:09:35 -0700

    C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * create_file - Creates a file and writes content to it
- * @filename: The name of the file to create
- * @text_content: The content to write to the file
- *
- * Return: 1 on success, -1 on failure
- */
-int create_file(const char *filename, char *text_content)
+* read_textfile- Read text file print to stdout.
+* @filename: text file being read
+* @letters: number of letters to be read
+* Return: w- actual number of bytes read and printed
+*        0 when function fails or filename is NULL.
+*
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd, len, res;
+char *buf;
+ssize_t fd;
+ssize_t w;
+ssize_t t;
 
-if (filename == NULL)
-return (-1);
-
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
-return (-1);
 
-if (text_content != NULL)
-{
-for (len = 0; text_content[len] != '\0'; len++)
-;
-res = write(fd, text_content, len);
-if (res == -1)
-return (-1);
-}
-
+return (0);
+buf = malloc(sizeof(char) * letters);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+free(buf);
 close(fd);
-return (1);
+return (w);
 }
-
