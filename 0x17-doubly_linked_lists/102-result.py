18f65965dc21065b89cd99bf2bb261fe9e699f23
Muluken <mulukeneshetu70@gmail.com>
2023-12-07 17:43:32 -0800

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
-def is_palindrome(n):
-    return str(n) == str(n)[::-1]
 
-def find_largest_palindrome():
-    largest_palindrome = 0
+products = (n * m for n in range(999, 99, -1) for m in range(n, 99, -1))
 
-    for i in range(100, 1000):
-        for j in range(100, 1000):
-            product = i * j
-            if is_palindrome(product) and product > largest_palindrome:
-                largest_palindrome = product
-
-    return largest_palindrome
-
-result = find_largest_palindrome()
-
-with open("102-result", "w") as file:
-    file.write(str(result))
+print(sorted(filter(lambda n: str(n) == str(n)[::-1], products))[-1])
