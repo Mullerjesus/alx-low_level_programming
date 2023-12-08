4c64f72a61e08ecc0b9e109c2f228e3055b9739d
Muluken <mulukeneshetu70@gmail.com>
2023-12-07 17:02:49 -0800

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,22 +1,19 @@
-#!/usr/bin/python3
-def is_palindrome(number):
-    # Convert the number to a string and check if it reads the same both ways
-    return str(number) == str(number)[::-1]
+!#/usr/bin/python3
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
 
-def largest_palindrome():
-    max_palindrome = 0
+def find_largest_palindrome():
+    largest_palindrome = 0
 
-    # Iterate through all possible products of two 3-digit numbers
     for i in range(100, 1000):
         for j in range(100, 1000):
             product = i * j
-            if is_palindrome(product) and product > max_palindrome:
-                max_palindrome = product
+            if is_palindrome(product) and product > largest_palindrome:
+                largest_palindrome = product
 
-    return max_palindrome
+    return largest_palindrome
 
-result = largest_palindrome()
+result = find_largest_palindrome()
 
-# Save the result to the file '102-result'
-with open('102-result', 'w') as file:
+with open("102-result", "w") as file:
     file.write(str(result))
